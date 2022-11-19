90f21ce747c53a38d34a73537bf8c8a9f91427cf
DeniseIra02 <irajubilo@gmail.com>
2022-11-19 14:39:54 +0800

    Set options in default female and normal. Added some text or label on the top of the options.

--- Text-to-Speech.py
+++ Text-to-Speech.py
@@ -26,10 +26,15 @@ Label(top_frame, text="TEXT CONVERT TO SPEECH", font="arial 20 bold", bg="white"
 text_input_area = Text(root, font="Robote 20", bg="#ab7fc7", relief=GROOVE, wrap=WORD)
 text_input_area.place(x=10, y=150, width=500, height=300)
 
+Label(root, text="VOICE", font="arial 15 bold", bg="#430b66", fg="white").place(x=580, y=160)
+Label(root, text="SPEED", font="arial 15 bold", bg="#430b66", fg="white").place(x=760, y=160)
+
 gender_combobox = Combobox(root, values=['Male', 'Female'], font="arial 14", state='r', width=10)
 gender_combobox.place(x=550, y=200)
+gender_combobox.set('Female')
 
 speed_combobox = Combobox(root, values=['Normal', 'Fast', 'Slow'], font="arial 14", state='r', width=10)
 speed_combobox.place(x=730, y=200)
+speed_combobox.set('Normal')
 
 root.mainloop()
