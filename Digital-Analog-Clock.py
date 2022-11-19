2bcb05bc95d846b7a58e9ed428634a3cda8b5fb4
DeniseIra02 <irajubilo@gmail.com>
2022-11-19 16:44:03 +0800

    At this point it is displaying the current time in digital.

--- Digital-Analog-Clock.py
+++ Digital-Analog-Clock.py
@@ -8,11 +8,12 @@ def current_clock():
     minutes = time.strftime("%M")
     seconds = time.strftime("%S")
     am_pm = time.strftime("%p ")
-    
     timetext= hours + ":" + minutes + ":" + seconds + ":" + " " + am_pm
     digital_clock.config(text=timetext)
 
 digital_clock = ui.Label(root_window, text="00:00:00", font="Helvetica 72 bold")
 digital_clock.pack()
+
+current_clock()
 
 root_window.mainloop()
