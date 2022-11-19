e387f03f5f73c4707eb21e82dea09040a2efe6e9
DeniseIra02 <irajubilo@gmail.com>
2022-11-19 12:53:55 +0800

    Declare a variable for the system to talk to the user.

--- VirtualAssistant.py
+++ VirtualAssistant.py
@@ -2,6 +2,11 @@ import speech_recognition as sr
 import pyttsx3
 
 __listener__ = sr.Recognizer()
+__engine__ = pyttsx3.init()
+
+__engine__.say("Hi, I am you Jazz")
+__engine__.say("Tell me what will I do for you")
+__engine__.runAndWait()
 
 try:
     with sr.Microphone() as source:
