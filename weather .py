7541da588b87c0810d22feb381f96a1718da85f1
DeniseIra02 <irajubilo@gmail.com>
2022-11-19 13:27:31 +0800

    Declare another 2 variable for the fonts of results or the labels in GUI application.

--- weather .py
+++ weather .py
@@ -9,7 +9,13 @@ canvas.title("Weather Forecast App")
 f = ("poppins", 15, "bold")
 t = ("poppins", 35, "bold")
 
-textfield = tk.Entry(canvas, font= t)
+textfield = tk.Entry(canvas, font = t)
 textfield.pack(pady=20)
 textfield.focus()
 
+label1 = tk.Label(canvas, font = t)
+label1.pack()
+label2 = tk.Label(canvas, font = f)
+label2.pack()
+
+canvas.mainloop()
