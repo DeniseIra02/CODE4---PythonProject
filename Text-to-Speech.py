d309848e1751306401bc4bc7da3857e0a9c0ce79
DeniseIra02 <irajubilo@gmail.com>
2022-11-19 13:57:47 +0800

    Declare a variable for the GUI app and then set its title and size.

--- Text-to-Speech.py
+++ Text-to-Speech.py
@@ -1,6 +1,13 @@
 import tkinter as tk
 from tkinter import *
 from tkinter import filedialog
-from tkinter.ttk import ComboBox
+from tkinter.ttk import Combobox
 import pyttsx3
 import os
+
+root = Tk()
+root.title("Text convert to Speech")
+root.geometry("1000x500")
+root.resizable(False, False)
+
+root.mainloop()
