7d1e39b8345d365148df1ad92a5a684855766dce
DeniseIra02 <irajubilo@gmail.com>
2022-11-19 16:41:36 +0800

    Imported time module. Added def functiom for the updated or current time.

--- Digital-Analog-Clock.py
+++ Digital-Analog-Clock.py
@@ -1,6 +1,16 @@
 import tkinter as ui 
+import time
 
 root_window = ui.Tk()
+
+def current_clock():
+    hours = time.strftime("%I")
+    minutes = time.strftime("%M")
+    seconds = time.strftime("%S")
+    am_pm = time.strftime("%p ")
+    
+    timetext= hours + ":" + minutes + ":" + seconds + ":" + " " + am_pm
+    digital_clock.config(text=timetext)
 
 digital_clock = ui.Label(root_window, text="00:00:00", font="Helvetica 72 bold")
 digital_clock.pack()
