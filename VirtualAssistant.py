cb4f0d008df067d30a653fc864fde6343983591b
DeniseIra02 <irajubilo@gmail.com>
2022-11-19 12:38:10 +0800

    Inserted a try block. Used microphone to become the source of audio. Declared a variable and set it to listen and recognize the source. Another variable to convert the speech recog to text.

--- VirtualAssistant.py
+++ VirtualAssistant.py
@@ -2,3 +2,12 @@ import speech_recognition as sr
 
 __listener__ = sr.Recognizer()
 
+try:
+    with sr.Microphone() as source:
+        print("Listening...")
+        voice = __listener__.listen(source)
+        command = __listener__.recognize_google(voice)
+        print(command)
+        
+except:
+    pass
